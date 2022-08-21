91d5a06b7e459468e9902375eeceaeb68917c7cd
Wastend <anatol-kovalencko2015@yandex.ru>
2022-08-21 14:00:50 +0200

    main_commit

---
+++ main.py
@@ -0,0 +1,18 @@
+'''
+создай в проекте функцию с обычным принтом вместо заглушки, я тоже в своей ветке
+'''
+
+
+def main():
+    galya() if input('who are you? ') == 'galya' else zhenya()
+
+
+def galya():
+    pass
+
+def zhenya():
+    pass
+
+
+if __name__ == 'main':
+    main()
